bcd4c265d2805b40b85b9b1c0e9bdff4c456feff
Mathieu Darse <mattdarse@gmail.com>
2013-01-17 10:32:33 +0100

    Add SF2 form support

--- src/app.php
+++ src/app.php
@@ -4,10 +4,12 @@ use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\FormServiceProvider;
 
 $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
+$app->register(new FormServiceProvider());
 $app->register(new TwigServiceProvider(), array(
     'twig.path'    => array(__DIR__.'/../templates'),
     // 'twig.options' => array('cache' => __DIR__.'/../cache'),
