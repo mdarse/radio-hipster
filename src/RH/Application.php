1cfb6c895b7d981cda96c6e4ece60f66e6c3b1d1
Mathieu Darse <mattdarse@gmail.com>
2013-02-05 02:05:19 +0100

    Revert traits modification from efca131bdb441056df1a06cd5157cf22e35775f5

--- src/RH/Application.php
+++ src/RH/Application.php
@@ -3,11 +3,10 @@
 namespace RH;
 
 use Silex\Application as BaseApplication;
-use \Silex\Application\UrlGeneratorTrait;
 
 class Application extends BaseApplication
 {
-    
+    use \Silex\Application\UrlGeneratorTrait;
 }
 
 
